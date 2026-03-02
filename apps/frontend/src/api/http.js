cfc11487028b899eb653c078329aee8bd0b21694
Carlos Grisales <carlose0320@hotmail.com>
2026-03-02 01:01:13 -0500

    fixing typo

--- apps/frontend/src/api/http.js
+++ apps/frontend/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const baseURL = import.meta.env.DEV
   ? '/api' // uses Vite proxy to localhost:3000
-  : import.meta.env.VI<TE_API_BASE_URL // API Gateway in production
+  : import.meta.env.VITE_API_BASE_URL // API Gateway in production
 
 export const http = axios.create({
   baseURL,
