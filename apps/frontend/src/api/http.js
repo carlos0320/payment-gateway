f47a25ef7d841da33d8bc68ff0b4c061d37e1ab2
Carlos Grisales <carlose0320@hotmail.com>
2026-03-01 23:00:37 -0500

    deploying frontend to s3+cloudfront and updating api back prod url

--- apps/frontend/src/api/http.js
+++ apps/frontend/src/api/http.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
+const baseURL = import.meta.env.DEV
+  ? '/api' // uses Vite proxy to localhost:3000
+  : import.meta.env.VI<TE_API_BASE_URL // API Gateway in production
 
 export const http = axios.create({
-  baseURL: '/api',
-  timeout: 15000,
+  baseURL,
+  headers: { 'Content-Type': 'application/json' },
 })
